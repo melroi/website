50ebbfb788ed9982dbe4438b6a511858b157a1f3
melroi <125811920+melroi@users.noreply.github.com>
2025-11-03 18:47:32 +0100

    11

---
+++ portfolio/script.js
@@ -0,0 +1,12 @@
+// Ouvre le menu latéral
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px"; // Largeur du menu
+}
+
+// Ferme le menu latéral
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
